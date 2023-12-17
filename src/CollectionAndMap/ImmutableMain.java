cb19ebc99691e61ce5620cc37381c0f29813225f
a93151127 <a93151127@gmail.com>
2023-12-17 14:29:40 +0800

    test home 1

--- src/CollectionAndMap/ImmutableMain.java
+++ src/CollectionAndMap/ImmutableMain.java
@@ -8,7 +8,7 @@ public class ImmutableMain {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 3, 5, 7);
         List<Integer> unModifiedList = Collections.unmodifiableList(list);
-
+        System.out.println("inside home");
         unModifiedList.forEach(System.out::println);
     }
 }
